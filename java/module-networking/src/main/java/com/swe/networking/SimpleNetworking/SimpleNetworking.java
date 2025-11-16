74c930ca105271d25501d6fb5d8ec58cf0696a29
LOGANATH07 <112201016@smail.iitpkd.ac.in>
2025-11-17 05:16:53 +0530

    SImple NEtworking bugfixes

--- java/module-networking/src/main/java/com/swe/networking/SimpleNetworking/SimpleNetworking.java
+++ java/module-networking/src/main/java/com/swe/networking/SimpleNetworking/SimpleNetworking.java
@@ -170,7 +170,7 @@ public final class SimpleNetworking
                 SimpleNetworkLogger.printError(MODULENAME, "Error on receiving data...");
             }
         }
-        SimpleNetworkLogger.printInfo(MODULENAME, "Thread exited from receiving data...");
+        SimpleNetworkLogger.printWarning(MODULENAME, "Thread exited from receiving data...");
     }
 
     /**
@@ -186,7 +186,7 @@ public final class SimpleNetworking
             SimpleNetworkLogger.printInfo(MODULENAME, "Added new subscriber : " + name.toString() + " ...");
             return;
         }
-        SimpleNetworkLogger.printInfo(MODULENAME, "The name " + name.toString() + " already exists...");
+        SimpleNetworkLogger.printError(MODULENAME, "The name " + name.toString() + " already exists...");
     }
 
     /**
